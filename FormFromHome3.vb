b55214a39d3b05aeb01502e6bc072267d1b59365
SunnyLo <yglo@gsitechnology.com.tw>
2014-07-25 00:58:04 +0800

    FromHome3 OK

---
+++ FormFromHome3.vb
@@ -0,0 +1,12 @@
+﻿Public Class FormFromHome3
+
+    Private Sub FormFromHome3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = "Hello World"
+    End Sub
+
+
+End Class
