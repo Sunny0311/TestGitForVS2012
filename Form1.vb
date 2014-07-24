b5b969a37f80df2fc516a695c04945d6b4adfe9e
Sunny Lo <yglo@gsitechnology.com.tw>
2014-07-24 17:03:57 +0800

    1st Commit Sunshine

---
+++ Form1.vb
@@ -0,0 +1,16 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("OK")
+
+        Dim a As String
+        a = "Hello World 3"
+        Dim b As Integer
+        b = 123
+
+    End Sub
+End Class
